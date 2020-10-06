dad833705c3b36b678cfa7a0a63502646bd3a97b
itsmeAmal <wickramarathna.amal@gmail.com>
2020-10-06 12:30:16 +0530

    remove old frontend project ,  add beetlogger initial spring boot running app, remove er folder from repo

---
+++ mpaylogger/src/main/java/com/mpaylogger/mpaylogger/MpayloggerApplication.java
@@ -0,0 +1,14 @@
+package com.mpaylogger.mpaylogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpayloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpayloggerApplication.class, args);
+		System.out.println("ok running.....");
+	}
+
+}
---
+++ mpaylogger/src/test/java/com/mpaylogger/mpaylogger/MpayloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpaylogger.mpaylogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpayloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
